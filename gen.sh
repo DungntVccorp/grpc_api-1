613003abbdf1c11a86d8156a55aad20b6b6f1d26
Dũng Nguyễn <dungnt@Dungs-Mac-mini-5.local>
2023-07-05 09:08:56 +0700

    update

--- gen.sh
+++ gen.sh
@@ -2,7 +2,5 @@
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 protoc -I=. --go_out=. api.proto
-protoc -I=. --go_out=. authen.api.proto
 protoc -I=. --go_out=. connect.api.proto
-protoc -I=. --go_out=. internal.api.proto
 protoc -I=. --go-grpc_out=. grpc.api.proto
